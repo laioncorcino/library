d8d56c52fd4232028013feb86f4cc49e59ef2c8c
Laion Corcino <laioncorcino@gmail.com>
2022-02-23 22:41:48 -0300

    test: tirando junit4 e add jupiter e assetj

--- src/test/java/com/corcino/library/unit/controller/BookControllerTest.java
+++ src/test/java/com/corcino/library/unit/controller/BookControllerTest.java
@@ -5,8 +5,8 @@ import com.corcino.library.dto.BookResponse;
 import com.corcino.library.service.BookService;
 import com.corcino.library.util.BookCreator;
 import com.corcino.library.util.JsonUtil;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.BDDMockito;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,14 +15,16 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@RunWith(SpringRunner.class)
+@ExtendWith(SpringExtension.class)
+@ActiveProfiles("test")
 @WebMvcTest
 @AutoConfigureMockMvc
 public class BookControllerTest {
@@ -41,11 +43,7 @@ public class BookControllerTest {
         BDDMockito.given(bookService.createBook(Mockito.any(BookRequest.class))).willReturn(bookPersisted);
 
         String bookRequestJson = JsonUtil.toJson(BookCreator.createBookGolangToBeSaved());
-
-        MockHttpServletRequestBuilder request = post(BOOK_API)
-                .contentType(MediaType.APPLICATION_JSON)
-                .accept(MediaType.APPLICATION_JSON)
-                .content(bookRequestJson);
+        MockHttpServletRequestBuilder request = configurePostRequest(bookRequestJson);
 
         mockMvc.perform(request)
                 .andExpect(status().isCreated())
@@ -55,11 +53,7 @@ public class BookControllerTest {
     @Test
     public void should_throw_a_validation_error_when_there_is_not_enough_data_to_create_the_book() throws Exception {
         String bookRequestJson = JsonUtil.toJson(new BookRequest());
-
-        MockHttpServletRequestBuilder request = post(BOOK_API)
-                .contentType(MediaType.APPLICATION_JSON)
-                .accept(MediaType.APPLICATION_JSON)
-                .content(bookRequestJson);
+        MockHttpServletRequestBuilder request = configurePostRequest(bookRequestJson);
 
         mockMvc.perform(request)
                 .andExpect(status().isBadRequest())
@@ -71,10 +65,7 @@ public class BookControllerTest {
         String bookRequestJson = JsonUtil.toJson(BookCreator.createBookGolangToBeSaved());
         BDDMockito.given(bookService.createBook(Mockito.any(BookRequest.class))).willThrow(new DataIntegrityViolationException("Isbn already used"));
 
-        MockHttpServletRequestBuilder request = post(BOOK_API)
-                .contentType(MediaType.APPLICATION_JSON)
-                .accept(MediaType.APPLICATION_JSON)
-                .content(bookRequestJson);
+        MockHttpServletRequestBuilder request = configurePostRequest(bookRequestJson);
 
         mockMvc.perform(request)
                 .andExpect(status().isConflict())
@@ -86,14 +77,18 @@ public class BookControllerTest {
         String bookRequestJson = JsonUtil.toJson(BookCreator.createBookGolangToBeSaved());
         BDDMockito.given(bookService.createBook(Mockito.any(BookRequest.class))).willThrow(new DataIntegrityViolationException("Title already used"));
 
-        MockHttpServletRequestBuilder request = post(BOOK_API)
-                .contentType(MediaType.APPLICATION_JSON)
-                .accept(MediaType.APPLICATION_JSON)
-                .content(bookRequestJson);
+        MockHttpServletRequestBuilder request = configurePostRequest(bookRequestJson);
 
         mockMvc.perform(request)
                 .andExpect(status().isConflict())
                 .andExpect(jsonPath("$.title").value("Title already used"));
+    }
+
+    private MockHttpServletRequestBuilder configurePostRequest(String bookRequestJson) {
+        return post(BOOK_API)
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .content(bookRequestJson);
     }
 
 }
--- src/test/java/com/corcino/library/unit/service/BookServiceTest.java
+++ src/test/java/com/corcino/library/unit/service/BookServiceTest.java
@@ -6,30 +6,27 @@ import com.corcino.library.model.Book;
 import com.corcino.library.repository.BookRepository;
 import com.corcino.library.service.BookService;
 import com.corcino.library.util.BookCreator;
-import org.junit.Before;
-import org.junit.Rule;
-import org.junit.Test;
-import org.junit.rules.ExpectedException;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.modelmapper.ModelMapper;
 import org.springframework.dao.DataIntegrityViolationException;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.catchThrowable;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 
-@RunWith(SpringRunner.class)
+@ExtendWith(SpringExtension.class)
+@ActiveProfiles("test")
 public class BookServiceTest {
 
     private ModelMapper mapper;
-
-    @Rule
-    public ExpectedException expectedException = ExpectedException.none();
 
     @InjectMocks
     private BookService bookService;
@@ -37,7 +34,7 @@ public class BookServiceTest {
     @Mock
     private BookRepository bookRepository;
 
-    @Before
+    @BeforeEach
     public void setUp() {
         mapper = new ModelMapper();
         bookService.setModelMapper(mapper);
@@ -47,37 +44,42 @@ public class BookServiceTest {
     public void should_save_book() throws Exception {
         Mockito.when(bookRepository.existsByIsbn(anyString())).thenReturn(false);
         Mockito.when(bookRepository.existsByTitle(anyString())).thenReturn(false);
-
         Mockito.when(bookRepository.save(any(Book.class))).thenReturn(mapper.map(BookCreator.createBookJavaPersisted(), Book.class));
 
         BookRequest bookRequest = BookCreator.createBookJavaToBeSaved();
         BookResponse response = bookService.createBook(bookRequest);
 
-        assertNotNull(response);
-        assertNotNull(response.getBookId());
-        assertEquals("Effective Java", response.getTitle());
+        assertThat(response).isNotNull();
+        assertThat(response.getBookId()).isNotNull();
+        assertThat(response.getTitle()).isEqualTo("Effective Java");
     }
 
     @Test
-    public void should_not_save_a_book_with_duplicated_isbn() throws Exception {
-        expectedException.expect(DataIntegrityViolationException.class);
-        expectedException.expectMessage("Isbn already used");
-
+    public void should_not_save_a_book_with_duplicated_isbn() {
         Mockito.when(bookRepository.existsByIsbn(anyString())).thenReturn(true);
 
         BookRequest bookRequest = BookCreator.createBookJavaToBeSaved();
-        bookService.createBook(bookRequest);
+        Throwable exception = catchThrowable(() -> bookService.createBook(bookRequest));
+
+        assertThat(exception)
+                .isInstanceOf(DataIntegrityViolationException.class)
+                .hasMessage("Isbn already used");
+
+        Mockito.verify(bookRepository, Mockito.never()).save(mapper.map(bookRequest, Book.class));
     }
 
     @Test
-    public void should_not_save_a_book_with_duplicated_title() throws Exception {
-        expectedException.expect(DataIntegrityViolationException.class);
-        expectedException.expectMessage("Title already used");
-
+    public void should_not_save_a_book_with_duplicated_title() {
         Mockito.when(bookRepository.existsByTitle(anyString())).thenReturn(true);
 
         BookRequest bookRequest = BookCreator.createBookJavaToBeSaved();
-        bookService.createBook(bookRequest);
+        Throwable exception = catchThrowable(() -> bookService.createBook(bookRequest));
+
+        assertThat(exception)
+                .isInstanceOf(DataIntegrityViolationException.class)
+                .hasMessage("Title already used");
+
+        Mockito.verify(bookRepository, Mockito.never()).save(mapper.map(bookRequest, Book.class));
     }
 
 }
